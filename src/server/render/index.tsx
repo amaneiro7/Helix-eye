da4c93cd80b713b54ee09e1628401f6fc01e385c
Andres Maneiro <amaneiro7@gmail.com>
2023-04-24 07:33:05 -0400

    clase 10, config de express para enviar el template de html desde el server

---
+++ src/server/render/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { App } from '../../app/containers/App'
+import { StaticRouter } from 'react-router-dom/server';
+import { renderToString } from 'react-dom/server';
+import { template } from './template';
+
+export const render = (url: string) => {
+    const stream = renderToString(
+        <StaticRouter location={url}>
+            <App />
+        </StaticRouter>)
+    return template(stream)    
+}
