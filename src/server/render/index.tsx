da6241c3538618eb239b9fa361b149a564ff0775
Andres Maneiro <amaneiro7@gmail.com>
2023-04-25 13:52:42 -0400

    inyectar styled component

--- src/server/render/index.tsx
+++ src/server/render/index.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { App } from '../../app/containers/App'
 import { StaticRouter } from 'react-router-dom/server';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 import { template } from './template';
 
 export const render = (url: string, initialProps={}) => {
-    const stream = renderToString(
-        <StaticRouter location={url}>
-            <App />
-        </StaticRouter>)
-    return template(stream, initialProps)
+    const sheet = new ServerStyleSheet()
+    try {
+        const stream = renderToString(
+            sheet.collectStyles(
+                <StaticRouter location={url}>
+                    <App />
+                </StaticRouter>))
+        const styleTags = sheet.getStyleTags()
+        return template(stream, initialProps, styleTags)
+    } catch (error) {
+        console.error(error);        
+    }
+
 }
